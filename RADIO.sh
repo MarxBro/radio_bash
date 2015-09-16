f6ac4df6de5a28287636aa6a6e85fa2a644bff93
MarxBro <allthemarxbrothers@gmail.com>
2015-09-16 18:10:36 -0300

    Inicial

---
+++ RADIO.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+######################################################################
+# RADIO.sh -> Radio in your terminal.
+######################################################################
+
+
+STATION_FILE="STATIONS.txt"
+
+function get_options_from_file {
+    LISTA=$( awk -F'@' '!/^#/ { OFS="@"; print $2 }' $STATION_FILE | sed 's/ /_/g')
+    #LISTA=$( awk -F'@' '!/^#/ { OFS=" "; print "\x27" $2 "\x27" }' $STATION_FILE )
+    echo $LISTA
+}
+
+function get_url_of_opt {
+    RADIO_URL=$( grep $1 $STATION_FILE | awk -F'@' '{print $1}' )
+    echo $RADIO_URL
+}
+
+function get_description {
+    RADIO_D=$( grep "$1" $STATION_FILE | awk -F'@' '{ print $3 }' )
+    echo $RADIO_D
+}
+
+declare -a OPTIONS=( "$(get_options_from_file $STATION_FILE | sed 's/@/ /g' )" )
+
+#echo ${OPTIONS[*]} # debug stuff
+
+#This is the best function I ever programmed.
+function line_draw {
+    LINE='----------------------------------'
+    echo $LINE
+}
+
+function do_menu {
+
+    clear &&
+    line_draw &&
+    echo 'Quit with CTRL+C.' &&
+    echo 'q for menu again.' 
+    line_draw &&
+    PS3="Your Choice: "
+
+    select opt in ${OPTIONS[@]}
+    do
+        opt_name=$( echo $opt | sed 's/_/ /g' )
+        URL=$(get_url_of_opt $opt_name)
+        DESCRIPTION=$(get_description $opt_name)
+        echo $DESCRIPTION && echo $URL && mplayer $URL &> /dev/null
+        do_menu
+    done 
+}
+
+do_menu
+exit 0;
+
+
+
+# Programado por MarxBro.
+    #Compartir con la licencia WTFPL.
---
+++ README.md
@@ -0,0 +1,29 @@
+#RADIO.sh
+
+## Español
+
+Simple script en bash para reproducir streaming-radio usando mplayer.
+
+Necesita el archivo STATIONS.
+
+Los valores en este último están separados por **@** y delimitan los siguientes campos:
+
+* URL
+* Nombre
+* Descripcion
+
+-------------------------------------------------------
+
+## English
+
+Simple bash script to play streaming radios using mplayer.
+
+It requires the STATIONS file. 
+
+Values in it are separated by **@**, delimiting fields as follow:
+
+* URL
+* NAME
+* Description
+
+
