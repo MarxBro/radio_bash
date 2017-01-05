62d79d74a2082aab7ac107e98e3096f5ad2ecfd1
MarxBro <allthemarxbroters@gmail.com>
2017-01-05 00:19:49 -0300

    dmenu radio

---
+++ dmenu_radio.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+######################################################################
+# While I figure a proper way to display the million stations available,
+# here is how I'm using this file:
+#
+#
+# This way we can use dmenu for search by keywords like "rock" or 
+# something...
+######################################################################
+
+dl_streaming(){
+    wget http://www.radiosure.com/rsdbms/stations2.zip &&
+    unzip *.zip
+    rm *.zip
+    RADIO_RSD=*.rsd
+    #echo $RADIO_RSD
+    perl -F"\t" -lae '$t="@". "@F[0]" . "@" . "@F[2]"; (@F[$_ * -1] =~ m/http/ ? print @F[$_ * -1], $t : "") foreach (1 .. 6)' < $RADIO_RSD  > STATIONS_HUGE.txt
+}
+play_stuff(){
+   mplayer $(cat STATIONS_HUGE.txt | dmenu | awk -F@ '{print $1}')
+}
+
+
+
+
+if [ -f STATIONS_HUGE.txt ]; then
+    play_stuff
+else
+    dl_streaming
+    play_stuff
+fi
+
+exit 0
+
+
+
